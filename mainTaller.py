5b9bb99a969bdf8c4049d209391a9a897656dcb2
Jessica cala <calajess@gmail.com>
2020-11-12 22:57:01 -0500

    cambios finales

--- mainTaller.py
+++ mainTaller.py
@@ -71,5 +71,37 @@ with result:
         rcontent = binding_set.getValue("rcontent")
         print("%s %s %s %s" % (message, content, date, rcontent))
 
+#querie: Devuelve los mensajes de los seguidores.
+query_string = """
+PREFIX  class: <http://example.com/class/>
+PREFIX  prop:  <http://example.com/property/> 
+PREFIX  rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX  xsd: <http://www.w3.org/2001/XMLSchema#>
+
+SELECT ?username ?userfollow ?f_user ?f_content ?f_date
+WHERE { ?user rdf:type class:User.
+?user prop:username ?username.
+?user prop:following ?followings.
+?followings prop:user ?userfollow.
+?userfollow prop:username ?f_user.
+?userfollow prop:writes ?f_message.
+?f_message prop:content ?f_content.
+?f_message prop:date ?f_date.       
+FILTER regex(?username, "alejandro@example.com", "i")}
+ORDER BY DESC(?f_date)"""
+
+print ("======================================================================================================================")
+print ("MENSAJES DE LOS SEGUIDORES")
+print ("======================================================================================================================")
+
+from franz.openrdf.query.query import QueryLanguage
+tuple_query = conn.prepareTupleQuery(QueryLanguage.SPARQL, query_string)
+result = tuple_query.evaluate()
+with result:
+    for binding_set in result:
+        f_user = binding_set.getValue("f_user")
+        f_content = binding_set.getValue("f_content")
+        f_date = binding_set.getValue("f_date")
+        print("%s %s %s" % (f_user, f_content, f_date))
 
 
